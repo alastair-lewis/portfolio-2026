d94fd23621fa17b1c61a8060877625111ca190ad
Alastair Lewis <33645568+alastair-lewis@users.noreply.github.com>
2026-03-09 09:32:04 -0400

    feat: wire up App and entry point (#16)
    App composes all sections in order: BackgroundFX, Nav, Hero, Projects,
    Experience, Education, About, Contact, Footer. main.tsx mounts into `#root`
    with StrictMode and imports the global palette.

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {App} from './App';
+import './styles/palette.css';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element #root not found in document.');
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
