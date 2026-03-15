557753eea1fe727d5ba4f87db6ff0014f8d67c13
Alastair Lewis <33645568+alastair-lewis@users.noreply.github.com>
2026-03-14 23:06:14 -0400

    Chore/correcting env variable names (#44)
    * correcting the posthog environment variable names
    
    * updating config to just set client

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {PostHogProvider} from '@posthog/react';
-import {POSTHOG_PROVIDER_CONFIG} from './analytics/posthog.config';
+import {posthogClient} from './analytics/posthog.config';
 
 import {App} from './App';
 import './i18n';
@@ -13,7 +13,7 @@ if (!rootElement) throw new Error('Root element #root not found in document.');
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <PostHogProvider {...POSTHOG_PROVIDER_CONFIG}>
+    <PostHogProvider client={posthogClient}>
       <App />
     </PostHogProvider>
   </React.StrictMode>,
