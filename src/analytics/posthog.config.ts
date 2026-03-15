36af2e0281ea3f72703b5ea97bab0be202d8ac27
alastair-lewis <alastair.lewis@shopify.com>
2026-03-14 23:04:51 -0400

    updating config to just set client

--- src/analytics/posthog.config.ts
+++ src/analytics/posthog.config.ts
@@ -1,7 +1,3 @@
-export const POSTHOG_PROVIDER_CONFIG = {
-  apiKey: import.meta.env.VITE_PUBLIC_POSTHOG_KEY,
-  options: {
-    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-    defaults: '2026-01-30',
-  } as const
-}
+import posthog from 'posthog-js'
+
+export const posthogClient = posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY)
